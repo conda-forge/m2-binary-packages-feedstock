925bb4c064bb75c7ff9661054eb07582081606f0
Isuru Fernando <isuruf@gmail.com>
2025-09-05 09:42:57 -0500

    fix unix

--- recipe/install_pkg.sh
+++ recipe/install_pkg.sh
@@ -1,10 +1,10 @@
 mkdir $PREFIX/Library
 cp -R $SRC_DIR/binary-${PKG_NAME}/* $PREFIX/Library/
 
-rm $PREFIX/Library/.INSTALL
-rm $PREFIX/Library/.BUILDINFO
-rm $PREFIX/Library/.MTREE
-rm $PREFIX/Library/.PKGINFO
+rm -f $PREFIX/Library/.INSTALL
+rm -f $PREFIX/Library/.BUILDINFO
+rm -f $PREFIX/Library/.MTREE
+rm -f $PREFIX/Library/.PKGINFO
 
 if [[ "${PKG_NAME}" == "m2-file" ]]; then
   rm -rf $PREFIX/Library/usr/lib/python3.11/site-packages/__pycache__/
@@ -17,4 +17,8 @@ fi
 if [[ "${PKG_NAME}" == "m2-ca-certificates" ]]; then
   mkdir $PREFIX/Scripts
   cp $RECIPE_DIR/.m2-ca-certificates-post-link.bat $PREFIX/Scripts/
+fi
+
+if [[ "${PKG_NAME}" == "m2-filesystem" ]]; then
+  echo "$PREFIX /conda_bin ntfs auto" >> ${PREFIX}/Library/etc/fstab
 fi
